3cc8a8b627581eb11a0b651154a430486c281aab
sanjeev0901 <76206355+sanjeev0901@users.noreply.github.com>
2021-04-24 12:25:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,86 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+var response,responseJSON,datetime,hour
+var bg;
+
+function preload() {
+    // create getBackgroundImg( ) here
+    getBackgroundImg();
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+
+    // add condition to check if any background image is there to add
+    if(backgroundImg)
+        background(backgroundImg);
+   
+
+
+    Engine.update(engine);
+
+    // write code to display time in correct format here
+    textSize(30);
+    fill("red");
+    
+    if(hour<12&&hour>0){
+        text("Time:"+hour+" AM",50,50);
+    }else{
+        text("Time:"+hour+" PM",50,50);
+    }
+
+}
+
+async function getBackgroundImg(){
+
+    // write code to fetch time from API
+     response=await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+
+    //change the data in JSON format
+     responseJSON=await response.json();
+
+    // write code slice the datetime
+     datetime=responseJSON.datetime;
+     hour=datetime.slice(11,13);
+
+    // add conditions to change the background images from sunrise to sunset
+    if(hour>=04&&hour<=06){
+        bg="sunrise1.png";
+    }else if(hour>=06&&hour<=08){
+        bg="sunrise2.png";
+    }else if(hour>=08&&hour<=10){
+        bg="sunrise3.png";
+    }else if(hour>=10&&hour<=12){
+        bg="sunrise4.png";
+    }else if(hour>=12&&hour<=14){
+        bg="sunrise5.png";
+    }else if(hour>=14&&hour<=16){
+        bg="sunrise6.png";
+    }else if(hour>=16&&hour<=18){
+        bg="sunrise7.png";
+    }else if(hour>=18&&hour<=20){
+        bg="sunrise8.png";
+    }else if(hour>=20&&hour<=22){
+        bg="sunrise9.png";
+    }else if(hour>=23&&hour<=0){
+        bg="sunrise10.png";
+    }else if(hour==0&&hour<=03){
+        bg="sunrise11.png";
+    }else {
+        bg="sunrise12.png";
+    }
+
+    //load the image in backgroundImg variable here
+    backgroundImg=loadImage(bg);
+}
